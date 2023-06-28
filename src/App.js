1cd3817bcff4f3c39788cf2ddf68d56811de9b79
Amkam <amaduskamara@gmail.com>
2023-06-28 13:57:58 +0000

    Add advice count view

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 const App = () => {
   const [advice, setAdvice] = useState();
+  const [adviceCount, setAdviceCount] = useState(0);
 
   const getAdvice = async () => {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setAdviceCount((c) => c + 1);
   };
 
   return (
@@ -16,7 +18,11 @@ const App = () => {
           React Advice App
         </h1>
         <div className="w-[700px] container mx-auto mt-10 rounded-xl p-10 bg-teal-50 shadow-xl">
-          <h3 className="text-center font-thin">{advice}</h3>
+          <h3 className="text-center text-xl">{advice}</h3>
+          <br />
+          <p className="text-sm">
+            You have read <span>{adviceCount}</span> pieces of advice
+          </p>
         </div>
 
         <div className="flex justify-center mt-16">
