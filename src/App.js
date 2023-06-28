46e050cfe55ae91096e6e2561bb470a1546faf0a
Amkam <amaduskamara@gmail.com>
2023-06-28 12:20:26 +0000

    Fetch and display advice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const App = () => {
+  const [advice, setAdvice] = useState();
+
+  const getAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  };
+
   return (
     <div>
-      <h1>React Advice App</h1>
+      <div class="container mx-auto py-16">
+        <h1 className="text-center text-3xl font-semibold leading-9 text-teal-400">
+          React Advice App
+        </h1>
+        <div className="w-[700px] container mx-auto mt-10 rounded-xl p-10 bg-teal-50 shadow-xl">
+          <h3 className="text-center font-thin">{advice}</h3>
+        </div>
+
+        <div className="flex justify-center mt-16">
+          <button
+            onClick={getAdvice}
+            className="py-4 cursor-pointer px-24 bg-teal-400 text-white text-lg"
+          >
+            Get Advice
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
